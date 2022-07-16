dcb28285ab28cb72a2cb88f5c17009b8250f1652
Amel9821 <amalia21198@gmail.com>
2022-07-16 13:09:09 +0700

    View arrow lengkap

--- NC3 SANA WatchKit Extension/ContentView.swift
+++ NC3 SANA WatchKit Extension/ContentView.swift
@@ -9,8 +9,81 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        ZStack {
+            VStack {
+                upArrow
+                Spacer()
+                downArrow
+            }
+            HStack {
+                leftArrow
+                Spacer()
+                rightArrow
+            }
+        }
+    }
+    
+    var upArrow: some View {
+        ZStack{
+            Circle()
+                .fill(Color("Ungu"))
+            Triangle()
+                .fill(Color("UnguTua"))
+                .frame(width: 30, height: 30)
+        }
+        .frame(width: 62, height: 62)
+       // .opacity(0.3)
+    }
+    var rightArrow: some View {
+        ZStack{
+            Circle()
+                .fill(Color("Ungu"))
+            Triangle()
+                .fill(Color("UnguTua"))
+                .frame(width: 30, height: 30)
+        }
+        .frame(width: 62, height: 62)
+        .opacity(0.3)
+        .rotationEffect(.degrees(90))
+    }
+    var leftArrow: some View {
+        ZStack{
+            Circle()
+                .fill(Color("Ungu"))
+            Triangle()
+                .fill(Color("UnguTua"))
+                .frame(width: 30, height: 30)
+        }
+        .frame(width: 62, height: 62)
+        .opacity(0.3)
+        .rotationEffect(.degrees(270))
+    }
+    var downArrow: some View {
+        ZStack{
+            Circle()
+                .fill(Color("Ungu"))
+            Triangle()
+                .fill(Color("UnguTua"))
+                .frame(width: 30, height: 30)
+        }
+        .frame(width: 62, height: 62)
+        .opacity(0.3)
+        .rotationEffect(.degrees(180))
+    }
+    
+    
+}
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
     }
 }
 

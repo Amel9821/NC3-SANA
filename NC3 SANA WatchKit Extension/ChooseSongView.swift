388b7b254ed48cdc38166628a6eb025456e8b851
ayumutiaradw <ayumutiaradw@gmail.com>
2022-07-20 18:06:12 +0700

    Update UI ChooseSongView

--- NC3 SANA WatchKit Extension/ChooseSongView.swift
+++ NC3 SANA WatchKit Extension/ChooseSongView.swift
@@ -20,7 +20,21 @@ struct ChooseSongView: View {
             Text("Choose your song")
             List{
                 NavigationLink(destination: SessionPageView(backToRoot: $backToRoot), isActive: self.$showSessionPageView, label: {
-                    Text("Bigbang - Fantastic Baby")
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundColor(Color("Ungu"))
+                            .frame(width: 195, alignment: .leading)
+                            .offset(x: -10)
+                        VStack(alignment: .leading) {
+                            Text("Bigbang")
+                                .font(.body)
+                            Text("Fantastic Baby")
+                                .font(.footnote)
+                        }
+                        .padding()
+                        .foregroundColor(Color("UnguTua"))
+                        .offset(x: -45)
+                    }
                         .onTapGesture {
                             self.showSessionPageView = true
                             AVService.shared.playMusic1()
@@ -28,7 +42,21 @@ struct ChooseSongView: View {
                 }
                 )
                 NavigationLink(destination: SessionPageView(backToRoot: $backToRoot), isActive: self.$showSessionPageView, label: {
-                    Text("Jesse - Zoom")
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundColor(Color("Ungu"))
+                            .frame(width: 195, alignment: .leading)
+                            .offset(x: -10)
+                        VStack(alignment: .leading) {
+                            Text("Jessi")
+                                .font(.body)
+                            Text("Zoom")
+                                .font(.footnote)
+                        }
+                        .padding()
+                        .foregroundColor(Color("UnguTua"))
+                        .offset(x: -73)
+                    }
                         .onTapGesture {
                             self.showSessionPageView = true
                             AVService.shared.playMusic2()
@@ -36,7 +64,21 @@ struct ChooseSongView: View {
                 }
                 )
                 NavigationLink(destination: SessionPageView(backToRoot: $backToRoot), isActive: self.$showSessionPageView, label: {
-                    Text("NCT - Beatbox")
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundColor(Color("Ungu"))
+                            .frame(width: 195, alignment: .leading)
+                            .offset(x: -10)
+                        VStack(alignment: .leading) {
+                            Text("NCT")
+                                .font(.body)
+                            Text("Beatbox")
+                                .font(.footnote)
+                        }
+                        .padding()
+                        .foregroundColor(Color("UnguTua"))
+                        .offset(x: -65)
+                    }
                         .onTapGesture {
                             self.showSessionPageView = true
                             AVService.shared.playMusic3()
@@ -44,7 +86,21 @@ struct ChooseSongView: View {
                 }
                 )
                 NavigationLink(destination: SessionPageView(backToRoot: $backToRoot), isActive: self.$showSessionPageView, label: {
-                    Text("Twice - What is Love")
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundColor(Color("Ungu"))
+                            .frame(width: 195, alignment: .leading)
+                            .offset(x: -10)
+                        VStack(alignment: .leading) {
+                            Text("Twice")
+                                .font(.body)
+                            Text("What is love?")
+                                .font(.footnote)
+                        }
+                        .padding()
+                        .foregroundColor(Color("UnguTua"))
+                        .offset(x: -50)
+                    }
                         .onTapGesture {
                             self.showSessionPageView = true
                             AVService.shared.playMusic4()
